d73fea10b09e5d906b4dfd40ecc9c3107890da53
dmontalto <dommontalto@outlook.com>
2024-02-14 16:25:48 +0700

    Tab view

---
+++ Appetizers/AppetizerListView.swift
@@ -0,0 +1,21 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Dominic Montalto on 14/02/2024.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        NavigationView {
+            Text("Appetizers")
+                .navigationTitle("Appetizers")
+        }
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
---
+++ Appetizers/OrderView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderView.swift
+//  Appetizers
+//
+//  Created by Dominic Montalto on 14/02/2024.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        NavigationView {
+            Text("Orders")
+                .navigationTitle("Orders")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+}
