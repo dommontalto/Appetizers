5adc2980337451b85d4b141897ba3cdc6c49fa5b
dmontalto <dommontalto@outlook.com>
2024-02-15 17:51:25 +0700

    Network manager

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Dominic Montalto on 15/02/2024.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
