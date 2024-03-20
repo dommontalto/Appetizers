71c7da5205654bac1ab1cb803b125338c243411b
dmontalto <dommontalto@outlook.com>
2024-03-20 17:04:40 +0700

    APError for async await

--- Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListViewModel.swift
@@ -23,7 +23,20 @@ import SwiftUI
                 appetizer = try await NetworkManager.shared.getAppetizers()
                 isLoading = false
             } catch {
-                alertItem = AlertContext.invalidResponse
+                if let apError = error as? APError {
+                    switch apError {
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
+                } else {
+                    alertItem = AlertContext.invalidResponse
+                }
                 isLoading = false
             }
         }
